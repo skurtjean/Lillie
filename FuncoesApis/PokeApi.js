f2ab1790a9e2da0c4c80131725493d1ba094372c
Jean <jean.jose@kbrtec.com.br>
2019-12-18 17:18:27 -0300

    Comando de exp

--- FuncoesApis/PokeApi.js
+++ FuncoesApis/PokeApi.js
@@ -25,7 +25,7 @@ function pokemon(name){
             url = data.sprites.front_default;
             resolve({
                 "title": title,
-                "description": type + "\n\n" + expBase + "\n" + EVP + "\n" + altura + "\n" + peso,
+                "description": type + "\n\n" + expBase + ", Utilize o comando !pokemonExp para ter um calculo do exp em determinado level\n" + EVP + "\n" + altura + "\n" + peso,
                 "thumbnail": {
                     "url": url,
                 },
@@ -48,37 +48,39 @@ function pokemon(name){
     });
 }
 
-function pokemonFull(name){
+function pokemonExp(name, level){
     return new Promise((resolve, reject) => {
         name = name.toLowerCase();
-        P.getPokemonSpeciesByName(name).then(function(data){
-            console.log(data);
-            title = firstLetterUp(data.forms[0].name) + ' National dex: ' + data.id;
-            description =
-            expBase = "Exp. base: " + data.base_experience;
-            altura = "Altura: " + data.height/10 + " metros";
-            peso = "Peso: " + data.weight/10 + " KG";
-            data.stats.forEach(function(item, index){
-                if(item.effort > 0){
-                    ev = item.effort;
-                    i = index;
+        P.getPokemonByName(name).then(function(data){
+            exp = (1*1*data.base_experience*1*level*1*1*1)/(7*1);
+            expVI = (1.5*1.7*data.base_experience*1.5*level*1.5*1*1.2)/(7*1);
+            expVII = (1*1.7*data.base_experience*1.5*level*1.5*1.2*1.2)/(7*1);
+            expMin = `O exp que um ${name} dará no level ${level} será: ${exp}, podendo sofrer alterações em gerações anteriores a VII.`; 
+            expMaxVI = `O máximo de exp que um ${name} dará no level ${level} na Gen VI será: ${expVI} (enfrentando um treinador, o-power ligado, seu pokemon é de uma troca internacional, está segurando um luck egg e já passou de seu level de evolução)`;
+            expMaxVII = `O máximo de exp que um ${name} dará no level ${level} na Gen VII será: ${expVII} (roto power ligado, seu pokemon é de uma troca internacional, está segurando um luck egg, já passou de seu level de evolução e possui 2 corações de afeição)`;
+            title = 'Exp de um ' + firstLetterUp(data.forms[0].name) + ' no level ' + level;
+            url = data.sprites.front_default;
+            resolve({
+                "title": title,
+                "description": expMin + '\n\n' + expMaxVI + '\n\n' + expMaxVII + '\n\n * OBS: Nas Gen VI e VII o Exp. share fará o pokemon no seu time que não entrou na luta receberá metade do exp que deveria, ou seja todo o calculo será feito mas dividirá o resultado por 2',
+                "thumbnail": {
+                    "url": url,
                 }
             });
-            EVP = "EV: " + ev + " pontos em " + firstLetterUp(data.stats[i].stat.name);
-            abilities = "";
-            data.abilities.forEach(function(item){
-                abilities += firstLetterUp(item.ability.name) + ((item.is_hidden)? " H.A." : "") + "\n";
-            });
-            status = "HP: " + data.stats[5].base_stat + "\nAtaque: " + data.stats[4].base_stat + "\nDefesa: " + data.stats[3].base_stat + "\nSpeed: " + data.stats[0].base_stat + "\nSp. ata.: " + data.stats[2].base_stat + "\nSp. def.: " + data.stats[1].base_stat;
-            url = data.sprites.front_default; 
-        })
-        .catch(function(err){
-            console.log(err);
-        })
+        }).catch(function(error){
+            console.log(error);
+        });
     });
+}
+
+function pokemonCatch(name){
+    return new Promise((resolve, reject) => {
+        name = name.toLowerCase();
+        P.getPokemon
+    })
 }
 
 function firstLetterUp(string){
     return string.substring(0,1).toUpperCase() + string.substring(1);
 }
-module.exports = { pokemon, pokemonFull }
+module.exports = { pokemon, pokemonExp }
--- Lillie.js
+++ Lillie.js
@@ -2,9 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const configs = require('./LillieConfig.json');
 const PokeFunction = require('./FuncoesApis/PokeApi');
+
 client.on('ready', function (evt) {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.users.get('439187903126700045').send(`Acabo de logar como ${client.user.tag}`);
 });
+
 
 client.on('message', function (message) {
     if (message.content.substring(0, 1) == '!') {
@@ -16,10 +19,15 @@ client.on('message', function (message) {
                     message.channel.send({embed: data});
                 });
             break;
-            case 'pokemon-full':
-                PokeFunction.pokemonFull(args[1]).then(function(data){
-                    message.author.send({embed: data});
-                });
+            case 'pokemonExp':
+                if(args.length < 3){
+                    message.channel.send('Por favor informe o level do pokemon');
+                }
+                else{
+                    PokeFunction.pokemonExp(args[1], args[2]).then(function(data){
+                        message.channel.send({embed: data});
+                    });
+                }
             break;
         }
     }
